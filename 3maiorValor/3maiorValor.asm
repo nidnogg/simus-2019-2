eeaf86d28b8763df5af689d35da27c8f66a0aa86
nidnogg <henriquevt98@gmail.com>
2019-09-02 17:42:04 -0300

    Adicionado arquivos .asm

---
+++ 3maiorValor/3maiorValor.asm
@@ -0,0 +1,3 @@
+; Escrever uma rotina ContaUnsZeros. Recebe o endereço de uma palavra de 32 bits na pilha 
+; e no acumulador um parâmetro, se for 0 conta o número de '0's, se for '1' conta o número de '1's na palavra.
+; O resultado é devolvido no acumulador. Apresente um programa de exemplo. 
